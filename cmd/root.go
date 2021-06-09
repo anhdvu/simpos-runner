9b297e91a443102a25b6f486812f7259e471d8b7
anhdvu <mrandyvu@gmail.com>
2021-06-10 02:30:31 +0700

    Moved files to simpos package.

--- cmd/root.go
+++ cmd/root.go
@@ -16,15 +16,12 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-	"os"
 	"github.com/spf13/cobra"
-
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
+	// homedir "github.com/mitchellh/go-homedir"
+	// "github.com/spf13/viper"
 )
 
-var cfgFile string
+// var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -48,38 +45,38 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
+	// cobra.OnInitialize(initConfig)
 
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.simposbot.yaml)")
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.simposbot.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		cobra.CheckErr(err)
+// func initConfig() {
+// 	if cfgFile != "" {
+// 		// Use config file from the flag.
+// 		viper.SetConfigFile(cfgFile)
+// 	} else {
+// 		// Find home directory.
+// 		home, err := homedir.Dir()
+// 		cobra.CheckErr(err)
 
-		// Search config in home directory with name ".simposbot" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".simposbot")
-	}
+// 		// Search config in home directory with name ".simposbot" (without extension).
+// 		viper.AddConfigPath(home)
+// 		viper.SetConfigName(".simposbot")
+// 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+// 	viper.AutomaticEnv() // read in environment variables that match
 
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
-}
+// 	// If a config file is found, read it in.
+// 	if err := viper.ReadInConfig(); err == nil {
+// 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+// 	}
+// }
--- go.mod
+++ go.mod
@@ -3,9 +3,7 @@ module github.com/anhdvu/simposbot
 go 1.16
 
 require (
-	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.3
-	github.com/spf13/viper v1.7.1
 	golang.org/x/net v0.0.0-20210525063256-abc453219eb5
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
